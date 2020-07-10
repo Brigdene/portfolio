149d312010fece00d7affbb22e15162c4df6b0cd
Brigdene <32480941+Brigdene@users.noreply.github.com>
2020-07-11 01:12:29 +0200

    Update master.js

--- master.js
+++ master.js
@@ -20,9 +20,9 @@ let projectName = {
 
 let portfolio = {
     name: "portfolio",
-    thumbnail: "https://github.com/Brigdene/portfolio/blob/master/assets/images/codeImgPortfolio.PNG",
+    thumbnail: "https://github.com/Brigdene/portfolio/blob/master/assets/images/websiteImgPortfolio.PNG?raw=true",
     linkToWebsite: "https://brigdene.github.io/portfolio/",
-    codeThumbnail: "/assets/images/codeImgPortfolio.PNG",
+    codeThumbnail: "https://github.com/Brigdene/portfolio/blob/master/assets/images/codeImgPortfolio.PNG?raw=true",
     codeImgIsDark: true,
     downloadableCode: "zip",
     linkToCode: "https://github.com/Brigdene/portfolio",
